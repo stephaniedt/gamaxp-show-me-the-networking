e0d34e71d1b480ce6ef8fb8c0cc6a7a0e2a5912c
andressalh <andressalhenriques@hotmail.com>
2020-05-19 14:33:14 -0300

    gif

--- js/countDown.js
+++ js/countDown.js
@@ -1,5 +1,5 @@
 
-var eventDate = new Date('May 20, 2020 12:00:00');
+var eventDate = new Date('May 20, 2020 18:00:00');
 var s = 1000;
 var m = s * 60;
 var h = m * 60;
@@ -19,10 +19,7 @@ function countdown(){
     var minutes =  Math.floor((remTime % h) / m);
     var seconds =  Math.floor((remTime % m) / s);
 
-    document.getElementById("divCd").innerHTML = (days+":"+hours+ ":" +minutes + ":" +seconds);
-
-    
-
+    document.getElementById("divCd").innerHTML = (('0' +days).slice(-2)+":"+('0' +hours).slice(-2)+ ":" +('0' +minutes).slice(-2) + ":" +('0' +seconds).slice(-2));
     
 }
 timer = setInterval(countdown, 1000);
