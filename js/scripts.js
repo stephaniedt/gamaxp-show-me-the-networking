4b7745f3f6eef655b580f375d5cc834c80114097
Stephanie <stephanie.diast@hotmail.com>
2020-05-20 17:07:10 -0300

    teste rdstation

--- js/scripts.js
+++ js/scripts.js
@@ -10,28 +10,28 @@ $('#logo').mouseleave(function(){
 
   document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
   
-  $('#formulario').submit(function (event) {
+//   $('#formulario').submit(function (event) {
     
-    var name = $('#name').val();
-    var email = $('#email').val();
-    var linkedin = $('#linkedin').val();
+//     var name = $('#name').val();
+//     var email = $('#email').val();
+//     var linkedin = $('#linkedin').val();
     
-    event.preventDefault();
+//     event.preventDefault();
     
-    $.ajax({
-      url: 'https://agenciareboot.us18.list-manage.com/subscribe/post',
-      method: 'POST',
-      data: { u: '8639194a63a0eeeb95aa17c8f', id: 'b4c1b7f0d5', NAME: name, EMAIL: email, LINKEDIN: linkedin },
-      dataType: 'jsonp',
-      function (response) {
-        console.log(response);
+//     $.ajax({
+//       url: 'https://agenciareboot.us18.list-manage.com/subscribe/post',
+//       method: 'POST',
+//       data: { u: '8639194a63a0eeeb95aa17c8f', id: 'b4c1b7f0d5', NAME: name, EMAIL: email, LINKEDIN: linkedin },
+//       dataType: 'jsonp',
+//       function (response) {
+//         console.log(response);
         
-      }
-    });
-});
+//       }
+//     });
+// });
 
-    $('#formulario').submit( function() {
-      alert("Dados enviados!")
-      window.location.href = "https://www.agenciareboot.com/thankyou.html";
+//     $('#formulario').submit( function() {
+//       alert("Dados enviados!")
+//       window.location.href = "https://www.agenciareboot.com/thankyou.html";
       
-    });
+//     });
