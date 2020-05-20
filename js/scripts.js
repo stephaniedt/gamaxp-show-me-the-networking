168da5c0fc1c744a46bcfa88f01e4596c39e801b
Stephanie <stephanie.diast@hotmail.com>
2020-05-20 00:06:22 -0300

    ....

--- js/scripts.js
+++ js/scripts.js
@@ -26,5 +26,5 @@ $('#logo').mouseleave(function(){
 });
 
 $('#formulario').submit(function () {
-  window.location = "https://www.agenciareboot.com.br/thankyou.html";
+  window.location = "https://www.agenciareboot.com/thankyou.html";
 });
