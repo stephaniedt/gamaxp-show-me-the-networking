1648a6dfbb46379221f4415af37d43de41224cdd
Stephanie <stephanie.diast@hotmail.com>
2020-05-19 22:31:35 -0300

    teste form

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,23 @@ $('#logo').mouseleave(function(){
       $('.popUpBox').hide();
   });
 
+  document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
   
   $('#formulario').submit(function (event) {
-
-  var name = $('#nickname').val();
-  var email = $('#email').val();
-  
-  event.preventDefault();
-
-  $.ajax({
+    
+    var name = $('#nickname').val();
+    var email = $('#email').val();
+    
+    event.preventDefault();
+    
+    $.ajax({
       url: 'https://agenciareboot.us18.list-manage.com/subscribe/post',
       method: 'POST',
       data: { u: '8639194a63a0eeeb95aa17c8f', id: 'b4c1b7f0d5', NICK: name, EMAIL: email },
       dataType: 'jsonp',
       success: function () {
-        window.location = "https://agenciareboot.com.br/thankyou.html"
+    
+        window.location = "https://agenciareboot.com.br/thankyou.html";
       }
-  });
+    });
 });
